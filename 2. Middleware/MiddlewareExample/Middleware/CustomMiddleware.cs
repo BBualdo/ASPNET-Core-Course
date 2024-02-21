8562305ac16c3364c53a1c0dba72fdd27e34d68c
Sebastian <mrseb10@gmail.com>
2024-02-21 20:49:01 +0100

    Custom Middleware Class

---
+++ 2. Middleware/MiddlewareExample/Middleware/CustomMiddleware.cs
@@ -0,0 +1,12 @@
+﻿namespace MiddlewareExample.Middleware
+{
+  public class CustomMiddleware : IMiddleware
+  {
+    async public Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+      await context.Response.WriteAsync("Custom middleware has been started!\n");
+      await next(context);
+      await context.Response.WriteAsync("Custom middleware has been ended!\n");
+    }
+  }
+}
--- 2. Middleware/MiddlewareExample/Program.cs
+++ 2. Middleware/MiddlewareExample/Program.cs
@@ -1,24 +1,25 @@
+using MiddlewareExample.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<CustomMiddleware>();
 var app = builder.Build();
 
 // middleware 1
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
-  await context.Response.WriteAsync("Hello\n");
+  await context.Response.WriteAsync("Middleware 1 - Start\n");
   await next(context);
+  await context.Response.WriteAsync("Middleware 1 - Stop\n");
 });
 
 // middleware 2
-app.Use(async (HttpContext context, RequestDelegate next) =>
-{
-  await context.Response.WriteAsync("Hello again!\n");
-  await next(context);
-});
+app.UseMiddleware<CustomMiddleware>();
 
 // middleware 3 (terminating middleware)
 app.Run(async (HttpContext context) =>
 {
-  await context.Response.WriteAsync("Hello one more time!\n");
+  await context.Response.WriteAsync("Middleware 3 - Start\n");
+  await context.Response.WriteAsync("Middleware 3 - Stop\n");
 });
 
 app.Run();
