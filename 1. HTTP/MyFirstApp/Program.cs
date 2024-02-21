0851e43e2f5da07ac7aadc854be39a3c85a9fdf9
Sebastian <mrseb10@gmail.com>
2024-02-21 15:39:57 +0100

    HTTP Post practice

--- 1. HTTP/MyFirstApp/Program.cs
+++ 1. HTTP/MyFirstApp/Program.cs
@@ -7,21 +7,21 @@ var app = builder.Build();
 app.Run(async (HttpContext context) =>
 {
   context.Response.ContentType = "text/html";
-  // need StreamReader, because Request.Body is type of Stream
+
   StreamReader reader = new StreamReader(context.Request.Body);
-  // Reading Stream and making it single string
+
   string body = await reader.ReadToEndAsync();
 
-  // converting string into Dictionary (Key=Value pairs)
-  // using StringValues because Keys in Query String are not unique, so if there is e.x. ?...age=20&age=30 both values can be shown
   Dictionary<string, StringValues> parsedQueryString = QueryHelpers.ParseQuery(body);
 
-  if (parsedQueryString.ContainsKey("firstName") && parsedQueryString.ContainsKey("age"))
+  // POST firstName=Asia&firstName=Sebastian
+
+  if (parsedQueryString.ContainsKey("firstName"))
   {
-    // In case if there is more than one value for one key I use first Index
-    string? firstName = parsedQueryString["firstName"][0];
-    string? age = parsedQueryString["age"][0];
-    await context.Response.WriteAsync($"<h1>{firstName} is {age} years old.</h1>");
+    foreach (string? firstName in parsedQueryString["firstName"])
+    {
+      await context.Response.WriteAsync($"<p>Hello, I'm {firstName}</p>");
+    }
   }
 });
 
