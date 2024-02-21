c99f5be15046f0d171364f41cdfaa650eebc4af7
Sebastian <mrseb10@gmail.com>
2024-02-21 15:37:44 +0100

    Request Headers

--- 1. HTTP/MyFirstApp/Program.cs
+++ 1. HTTP/MyFirstApp/Program.cs
@@ -4,16 +4,12 @@ var app = builder.Build();
 app.Run(async (HttpContext context) =>
 {
   context.Response.Headers["Content-Type"] = "text/html";
-  if (context.Request.Method == "GET")
+  if (context.Request.Headers.ContainsKey("User-Agent"))
   {
-    string? id = context.Request.Query["id"];
-    await context.Response.WriteAsync($"<h1>{id}</h1>");
-
-    string? name = context.Request.Query["name"];
-    await context.Response.WriteAsync($"<h2>{name}</h2>");
-
-    // localhost:5000?id=1337&name=Sebastian
+    string? userAgent = context.Request.Headers["User-Agent"];
+    await context.Response.WriteAsync($"<h1>{userAgent}</h1>");
   }
+
 });
 
 app.Run();
