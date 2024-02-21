775c8a06b76fdfbcbba461be2fa6ebbf65488f22
Sebastian <mrseb10@gmail.com>
2024-02-21 15:34:15 +0100

    HTTP Status Codes

--- 1. HTTP/MyFirstApp/Program.cs
+++ 1. HTTP/MyFirstApp/Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+  // 1xx Informational: 
+  //    101 - Switching Protocols
+  // 2xx Success:
+  //    200 - OK
+  // 3xx Redirection:
+  //    302 - Found
+  //    304 - Not Modified
+  // 4xx Client error:
+  //    400 - Bad Request
+  //    401 - Unauthorized
+  //    404 - Not Found
+  // 5xx Server error:
+  //    500 Internal Server Error
+  context.Response.StatusCode = 400;
+  await context.Response.WriteAsync("Hello");
+  await context.Response.WriteAsync("World");
+});
 
 app.Run();
