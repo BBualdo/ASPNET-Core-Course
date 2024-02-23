e9bfbfee0f20414cb3e0f5a4e1c5a138929bf747
Sebastian <mrseb10@gmail.com>
2024-02-23 19:40:01 +0100

    Map, MapGet, MapPost

--- 3. Routing/RoutingExample/Program.cs
+++ 3. Routing/RoutingExample/Program.cs
@@ -8,7 +8,27 @@ app.UseRouting();
 app.UseEndpoints(endpoints =>
 {
   // add your endpoints
+  endpoints.Map("home", async (context) =>
+  {
+    await context.Response.WriteAsync("Home page");
+  });
+
+  // Executes only for GET HTTP method
+  endpoints.MapGet("about", async (context) =>
+  {
+    await context.Response.WriteAsync("About page");
+  });
+
+  // Executes only for POST HTTP method
+  endpoints.MapPost("randompage", async context =>
+  {
+    await context.Response.WriteAsync("Random Page");
+  });
 });
 
+app.Run(async context =>
+{
+  await context.Response.WriteAsync($"Request received at: {context.Request.Path}");
+});
 
 app.Run();
