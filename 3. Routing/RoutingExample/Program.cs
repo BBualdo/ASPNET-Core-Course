29bb1b491188b6894152fca00021722c1ab9f1b4
Sebastian <mrseb10@gmail.com>
2024-02-23 20:19:30 +0100

    GetEndpoint

--- 3. Routing/RoutingExample/Program.cs
+++ 3. Routing/RoutingExample/Program.cs
@@ -1,30 +1,52 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+  // endpoint will be null here, because it is called before UseRouting()
+  Endpoint? endpoint = context.GetEndpoint();
+  if (endpoint != null)
+  {
+    await context.Response.WriteAsync($"Endpoint: {endpoint.DisplayName}\n");
+  }
+  await next();
+});
+
 // enable routing
 app.UseRouting();
 
-// creating endpoints
-app.UseEndpoints(endpoints =>
+app.Use(async (context, next) =>
 {
-  // add your endpoints
-  endpoints.Map("home", async (context) =>
+  Endpoint? endpoint = context.GetEndpoint();
+  if (endpoint != null)
+  {
+    await context.Response.WriteAsync($"Endpoint: {endpoint.DisplayName}\n");
+  }
+  await next();
+});
+
+// creating endpoints (optional in top-level statement)
+app.UseEndpoints(endpoints =>
+{ });
+
+// add your endpoints
+app.Map("home", async (context) =>
   {
     await context.Response.WriteAsync("Home page");
   });
 
-  // Executes only for GET HTTP method
-  endpoints.MapGet("about", async (context) =>
-  {
-    await context.Response.WriteAsync("About page");
-  });
-
-  // Executes only for POST HTTP method
-  endpoints.MapPost("randompage", async context =>
-  {
-    await context.Response.WriteAsync("Random Page");
-  });
+// Executes only for GET HTTP method
+app.MapGet("about", async (context) =>
+{
+  await context.Response.WriteAsync("About page");
 });
+
+// Executes only for POST HTTP method
+app.MapPost("randompage", async context =>
+{
+  await context.Response.WriteAsync("Random Page");
+});
+
 
 app.Run(async context =>
 {
