6a94be3d2780d2109cd76ae2e0421134b55d011c
Sebastian <mrseb10@gmail.com>
2024-02-23 05:19:58 +0100

    Intro to Routing

---
+++ 3. Routing/RoutingExample/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// enable routing
+app.UseRouting();
+
+// creating endpoints
+app.UseEndpoints(endpoints =>
+{
+  // add your endpoints
+});
+
+
+app.Run();
