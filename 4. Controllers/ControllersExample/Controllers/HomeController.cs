b16cfcb648b1eb07a1d76a1e2527871887ecd0bc
Sebastian <mrseb10@gmail.com>
2024-02-24 22:06:35 +0100

    Creating Controllers

---
+++ 4. Controllers/ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersExample.Controllers
+{
+  public class HomeController
+  {
+    [Route("sayhello")] // Attribute Routing
+    public string Method1()
+    {
+      return "Hello from HomeController.Method1";
+    }
+  }
+}
---
+++ 4. Controllers/ControllersExample/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers(); // adds all controller classes as services
+var app = builder.Build();
+
+// app.UseRouting();
+// app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+// you can shortcut it with:
+
+app.MapControllers();
+
+app.Run();
