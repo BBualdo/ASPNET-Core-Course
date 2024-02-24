da9b534f8a4106de0c92f533c84aa0ee39b8d146
Sebastian <mrseb10@gmail.com>
2024-02-24 08:30:30 +0100

    Optional Parameters

--- 3. Routing/RoutingExample/Program.cs
+++ 3. Routing/RoutingExample/Program.cs
@@ -16,16 +16,32 @@ app.UseEndpoints(endpoints =>
     await context.Response.WriteAsync($"Accessing file: {fileName}.{extension}");
   });
 
-  endpoints.Map("employee/profile/{employeename}", async context =>
+  endpoints.Map("employee/profile/{employeename?}", async context =>
   {
-    string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
-    await context.Response.WriteAsync($"Accesing {employeeName} employee profile.");
+    if (context.Request.RouteValues.ContainsKey("employeename"))
+    {
+      string? employeeName = Convert.ToString(context.Request.RouteValues["employeename"]);
+      await context.Response.WriteAsync($"Accesing {employeeName} employee profile.");
+    }
+    else
+    {
+      await context.Response.WriteAsync("Please enter employee name.");
+    }
+
   });
 
-  endpoints.Map("products/details/{id=1}", async context =>
+  endpoints.Map("products/details/{id?}", async context =>
   {
-    int? id = Convert.ToInt32(context.Request.RouteValues["id"]);
-    await context.Response.WriteAsync($"Details about Product with ID: {id}");
+    if (context.Request.RouteValues.ContainsKey("id"))
+    {
+      int? id = Convert.ToInt32(context.Request.RouteValues["id"]);
+      await context.Response.WriteAsync($"Details about Product with ID: {id}");
+    }
+    else
+    {
+      await context.Response.WriteAsync("No valid Product ID was provided.");
+    }
+
   });
 });
 #pragma warning restore ASP0014 // Suggest using top level route registrations
