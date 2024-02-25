511c615e89a96a490cb7c3b0be776e47a87bf87b
Sebastian <mrseb10@gmail.com>
2024-02-25 07:34:23 +0100

    Multiple Action Methods

--- 4. Controllers/ControllersExample/Controllers/HomeController.cs
+++ 4. Controllers/ControllersExample/Controllers/HomeController.cs
@@ -4,10 +4,24 @@ namespace ControllersExample.Controllers
 {
   public class HomeController
   {
-    [Route("sayhello")] // Attribute Routing
-    public string Method1()
+    [Route("/")] // Attribute Routing
+    [Route("home")]
+    public string Index()
     {
-      return "Hello from HomeController.Method1";
+      return "Hello from Home Page";
+    }
+
+
+    [Route("about")]
+    public string About()
+    {
+      return "Hello from About Page";
+    }
+
+    [Route("contact-us/{mobile:regex(^\\d{{10}}$)}")]
+    public string Contact()
+    {
+      return "Hello from Contact Page";
     }
   }
 }
