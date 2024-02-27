5eb9bbd0e63e26ea91da7b6f86d1ae200779cd34
Sebastian <mrseb10@gmail.com>
2024-02-27 09:13:40 +0100

    Using the same example as in Controllers

---
+++ 5. Models/IActionResultExample/Controllers/StoreController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelBindingExample.Controllers
+{
+  public class StoreController : Controller
+  {
+    // This is new URL that will be passed to Response Headers when trying to Request /books
+    [Route("store/books/{id}")]
+    public IActionResult Books()
+    {
+      int id = Convert.ToInt32(Request.RouteValues["id"]);
+
+      return Content($"<h1>Book Store</h1> <p>{id}</p>", "text/html");
+    }
+  }
+}
