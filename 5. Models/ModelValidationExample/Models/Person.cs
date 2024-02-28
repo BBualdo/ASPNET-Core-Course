401d95678f8ff5a6d39b42483f0446670bdddc74
Sebastian <mrseb10@gmail.com>
2024-02-28 19:17:17 +0100

    All Model Validations - 1/2

--- 5. Models/ModelValidationExample/Models/Person.cs
+++ 5. Models/ModelValidationExample/Models/Person.cs
@@ -4,13 +4,17 @@ namespace ModelValidationExample.Models
 {
   public class Person
   {
-    [Required(ErrorMessage = "Please enter your name.")] // This is model validation attribute
+    [Required(ErrorMessage = "{0} is required.")] // This is model validation attribute
+    [Display(Name = "Person Name")]
+    [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long.")]
     public string Name { get; set; }
     [Required]
     public string? Email { get; set; }
     public string? Phone { get; set; }
     public string? Password { get; set; }
     public string? ConfirmPassword { get; set; }
+
+    [Range(0, 999.99, ErrorMessage = "{0} should be between ${1} and ${2}.")]
     public double? Price { get; set; }
 
 
