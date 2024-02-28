7c31bfaea42039f0fded4ee051c13948a34bc39c
Sebastian <mrseb10@gmail.com>
2024-02-28 19:43:58 +0100

    All Model Validations - 2/2

--- 5. Models/ModelValidationExample/Models/Person.cs
+++ 5. Models/ModelValidationExample/Models/Person.cs
@@ -7,11 +7,19 @@ namespace ModelValidationExample.Models
     [Required(ErrorMessage = "{0} is required.")] // This is model validation attribute
     [Display(Name = "Person Name")]
     [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} should be between {2} and {1} characters long.")]
-    public string Name { get; set; }
+    [RegularExpression("^[A-Za-z .]*$", ErrorMessage = "{0} should contain only letters, space and dot.")]
+    public string? Name { get; set; }
     [Required]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
     public string? Email { get; set; }
+    [Phone(ErrorMessage = "Invalid phone number.")]
+    //[ValidateNever]
     public string? Phone { get; set; }
+    [Required(ErrorMessage = "{0} is required.")]
     public string? Password { get; set; }
+    [Required(ErrorMessage = "{0} is required.")]
+    [Display(Name = "Password confirmation")]
+    [Compare("Password", ErrorMessage = "Passwords do not match.")]
     public string? ConfirmPassword { get; set; }
 
     [Range(0, 999.99, ErrorMessage = "{0} should be between ${1} and ${2}.")]
