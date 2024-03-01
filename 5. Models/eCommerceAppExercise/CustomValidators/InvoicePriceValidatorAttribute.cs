f3381bf12b6b089d663de3a71fed1feeac15d0a7
Sebastian <mrseb10@gmail.com>
2024-03-01 18:56:55 +0100

    Tried again

--- 5. Models/eCommerceAppExercise/CustomValidators/InvoicePriceValidatorAttribute.cs
+++ 5. Models/eCommerceAppExercise/CustomValidators/InvoicePriceValidatorAttribute.cs
@@ -6,49 +6,43 @@ namespace eCommerceAppExercise.CustomValidators
 {
   public class InvoicePriceValidatorAttribute : ValidationAttribute
   {
-
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
       if (value != null)
       {
-        // Getting property
         PropertyInfo? productsProperty = validationContext.ObjectType.GetProperty(nameof(Order.Products));
 
         if (productsProperty != null)
         {
-          // Setting products as value of property
           List<Product> products = (List<Product>)productsProperty.GetValue(validationContext.ObjectInstance)!;
 
           double totalPrice = 0;
+
           foreach (Product product in products)
           {
-            // Calculating total price
             totalPrice += product.Price * product.Quantity;
           }
 
-          // Actual Price from property which has Custom Attribute
-          double actualPrice = (double)value!;
-
           if (totalPrice > 0)
           {
+            double actualPrice = (double)value;
+
             if (actualPrice != totalPrice)
             {
-              return new ValidationResult("Invoice Price should be equal to all product's price and quantities.");
+              return new ValidationResult("The InvoicePrice is not equal to calculated price.");
             }
-
           }
           else
           {
-            return new ValidationResult("No products has been found to calculate price.");
+            return new ValidationResult("Not found any products to calculate.");
           }
 
           return ValidationResult.Success;
-        }
 
+        }
         return null;
       }
       return null;
     }
-
   }
 }
