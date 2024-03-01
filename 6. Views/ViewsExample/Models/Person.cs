6f12c88361141e149fd602aaf271bbf82b22f0a0
Sebastian <mrseb10@gmail.com>
2024-03-01 22:38:36 +0100

    Razor: If-else

--- 6. Views/ViewsExample/Models/Person.cs
+++ 6. Views/ViewsExample/Models/Person.cs
@@ -1,14 +1,19 @@
 ﻿namespace ViewsExample.Models
 {
-  public class Person
-  {
-    public string Name { get; set; }
-    public DateTime DateOfBirth { get; set; }
-
-    public Person(string name, DateTime dateOfBirth)
+    public class Person
     {
-      Name = name;
-      DateOfBirth = dateOfBirth;
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public Person(string name, DateTime? dateOfBirth)
+        {
+            Name = name;
+            DateOfBirth = dateOfBirth;
+        }
     }
-  }
 }
