b0313661897f50c19822060d98395edb25a61128
Sebastian <mrseb10@gmail.com>
2024-03-02 20:27:30 +0100

    ViewData - 1/2

--- 6. Views/ViewsExample/Controllers/HomeController.cs
+++ 6. Views/ViewsExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewsExample.Models;
 
 namespace ViewsExample.Controllers
 {
@@ -8,7 +9,16 @@ namespace ViewsExample.Controllers
         [Route("/")]
         public IActionResult Index()
         {
-            return View(); // searches for Views/Home/Index.cshtml
+            ViewData["appTitle"] = "ASP.NET Core App";
+            List<Person> people = new List<Person>
+            {
+                new Person("Sebastian", new DateTime(2000, 05, 23), Person.Gender.Male),
+                new Person("Asia", new DateTime(1999, 04, 30), Person.Gender.Female),
+                new Person("Dominik", new DateTime(2024, 03, 02), Person.Gender.Male)
+            };
+            ViewData["people"] = people;
+
+            return View();
         }
     }
 }
