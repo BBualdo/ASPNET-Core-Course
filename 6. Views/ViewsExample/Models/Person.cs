8bc68af00466af01f17fefe695d574738f457dc6
Sebastian <mrseb10@gmail.com>
2024-03-02 09:47:49 +0100

    Foreach

--- 6. Views/ViewsExample/Models/Person.cs
+++ 6. Views/ViewsExample/Models/Person.cs
@@ -4,16 +4,19 @@
     {
         public string Name { get; set; }
         public DateTime? DateOfBirth { get; set; }
+        public List<string> Hobbies { get; set; }
 
         public Person(string name)
         {
             Name = name;
         }
 
-        public Person(string name, DateTime? dateOfBirth)
+        public Person(string name, DateTime? dateOfBirth, List<string> hobbies)
         {
             Name = name;
             DateOfBirth = dateOfBirth;
+            Hobbies = hobbies;
+            Hobbies = hobbies;
         }
     }
 }
