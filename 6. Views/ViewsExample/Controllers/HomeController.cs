cf0daafd645fcd1efd9ec8622decb4f70e50c56f
Sebastian <mrseb10@gmail.com>
2024-03-03 00:16:24 +0100

    ViewData - 2/2

--- 6. Views/ViewsExample/Controllers/HomeController.cs
+++ 6. Views/ViewsExample/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace ViewsExample.Controllers
             {
                 new Person("Sebastian", new DateTime(2000, 05, 23), Person.Gender.Male),
                 new Person("Asia", new DateTime(1999, 04, 30), Person.Gender.Female),
-                new Person("Dominik", new DateTime(2024, 03, 02), Person.Gender.Male)
+                new Person("Dominik", DateTime.Now, Person.Gender.Male)
             };
             ViewData["people"] = people;
 
