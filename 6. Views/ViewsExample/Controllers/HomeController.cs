cbe4f7ee2d2b4dd339be050372ce6e0355a51ddc
Sebastian <mrseb10@gmail.com>
2024-03-03 09:00:43 +0100

    Table beautify and Strongly Typed Views 1/2

--- 6. Views/ViewsExample/Controllers/HomeController.cs
+++ 6. Views/ViewsExample/Controllers/HomeController.cs
@@ -3,23 +3,22 @@ using ViewsExample.Models;
 
 namespace ViewsExample.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    [Route("home")]
+    [Route("/")]
+    public IActionResult Index()
     {
-        [Route("home")]
-        [Route("/")]
-        public IActionResult Index()
+      ViewData["appTitle"] = "ASP.NET Core App";
+      List<Person> people = new List<Person>
         {
-            ViewData["appTitle"] = "ASP.NET Core App";
-            List<Person> people = new List<Person>
-            {
-                new Person("Sebastian", new DateTime(2000, 05, 23), Person.Gender.Male),
-                new Person("Asia", new DateTime(1999, 04, 30), Person.Gender.Female),
-                new Person("Dominik", DateTime.Now, Person.Gender.Male)
-            };
-            // ViewData["people"] = people;
-            ViewBag.people = people;
-
-            return View();
-        }
+          new Person("Sebastian", new DateTime(2000, 05, 23), Person.Gender.Male),
+          new Person("Asia", new DateTime(1999, 04, 30), Person.Gender.Female),
+          new Person("Dominik", DateTime.Now, Person.Gender.Male)
+        };
+      // ViewData["people"] = people;
+      //ViewBag.people = people;
+      return View(people);
     }
+  }
 }
--- 6. Views/ViewsExample/Models/Person.cs
+++ 6. Views/ViewsExample/Models/Person.cs
@@ -1,26 +1,26 @@
 ﻿namespace ViewsExample.Models
 {
-    public class Person
+  public class Person
+  {
+    public string Name { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public Gender PersonGender { get; set; }
+
+    public Person(string name)
     {
-        public string Name { get; set; }
-        public DateTime? DateOfBirth { get; set; }
-        public Gender PersonGender { get; set; }
-
-        public Person(string name)
-        {
-            Name = name;
-        }
-
-        public Person(string name, DateTime? dateOfBirth, Gender gender)
-        {
-            Name = name;
-            DateOfBirth = dateOfBirth;
-            PersonGender = gender;
-        }
-
-        public enum Gender
-        {
-            Male, Female, Other
-        }
+      Name = name;
     }
+
+    public Person(string name, DateTime? dateOfBirth, Gender gender)
+    {
+      Name = name;
+      DateOfBirth = dateOfBirth;
+      PersonGender = gender;
+    }
+
+    public enum Gender
+    {
+      Male, Female, Other
+    }
+  }
 }
