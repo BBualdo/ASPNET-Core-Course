2744692215349804481b1e5df81a407b8303caeb
Sebastian <mrseb10@gmail.com>
2024-03-03 13:10:27 +0100

    Strongly Typed Views with Multiple Models

--- 6. Views/ViewsExample/Controllers/HomeController.cs
+++ 6. Views/ViewsExample/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace ViewsExample.Controllers
     [Route("/")]
     public IActionResult Index()
     {
-      ViewData["appTitle"] = "ASP.NET Core App";
+      ViewBag.appTitle = "ASP.NET Core Web App | People";
       List<Person> people = new List<Person>
         {
           new Person("Sebastian", new DateTime(2000, 05, 23), Person.Gender.Male),
@@ -25,6 +25,8 @@ namespace ViewsExample.Controllers
     [Route("details/{name}")]
     public IActionResult Details(string? name)
     {
+      ViewBag.appTitle = "ASP.NET Core Web App | Details";
+
       if (name == null)
       {
         return Content("Name can't be empty");
@@ -41,6 +43,30 @@ namespace ViewsExample.Controllers
       Person? matchingPerson = people.Where(person => person.Name == name).FirstOrDefault();
 
       return View(matchingPerson);
+    }
+
+    [Route("wishlist")]
+    public IActionResult WishList()
+    {
+      ViewBag.appTitle = "ASP.NET Core Web App | People and Products";
+
+      List<Person> people = new List<Person>
+        {
+          new Person("Sebastian", new DateTime(2000, 05, 23), Person.Gender.Male),
+          new Person("Asia", new DateTime(1999, 04, 30), Person.Gender.Female),
+          new Person("Dominik", DateTime.Now, Person.Gender.Male),
+          new Person("Frania", new DateTime(1945, 09, 15), Person.Gender.Female),
+        };
+
+      List<Product> wishList = new List<Product>
+      {
+        new Product("Car", "Red"),
+        new Product("Headphones", "Black"),
+        new Product("Holidays", "Dreamy")
+      };
+
+      PersonAndProductWrapper wrapper = new PersonAndProductWrapper(people, wishList);
+      return View(wrapper);
     }
   }
 }
