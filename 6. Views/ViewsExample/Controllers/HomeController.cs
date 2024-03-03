9910ce7c041e9f6226e5f8012ab86dcc56bbc32c
Sebastian <mrseb10@gmail.com>
2024-03-03 11:36:28 +0100

    Strongly Typed Views 2/2

--- 6. Views/ViewsExample/Controllers/HomeController.cs
+++ 6. Views/ViewsExample/Controllers/HomeController.cs
@@ -14,11 +14,33 @@ namespace ViewsExample.Controllers
         {
           new Person("Sebastian", new DateTime(2000, 05, 23), Person.Gender.Male),
           new Person("Asia", new DateTime(1999, 04, 30), Person.Gender.Female),
-          new Person("Dominik", DateTime.Now, Person.Gender.Male)
+          new Person("Dominik", DateTime.Now, Person.Gender.Male),
+          new Person("Frania", new DateTime(1945, 09, 15), Person.Gender.Female),
         };
       // ViewData["people"] = people;
       //ViewBag.people = people;
       return View(people);
+    }
+
+    [Route("details/{name}")]
+    public IActionResult Details(string? name)
+    {
+      if (name == null)
+      {
+        return Content("Name can't be empty");
+      }
+
+      List<Person> people = new List<Person>
+        {
+          new Person("Sebastian", new DateTime(2000, 05, 23), Person.Gender.Male),
+          new Person("Asia", new DateTime(1999, 04, 30), Person.Gender.Female),
+          new Person("Dominik", DateTime.Now, Person.Gender.Male),
+          new Person("Frania", new DateTime(1945, 09, 15), Person.Gender.Female),
+        };
+
+      Person? matchingPerson = people.Where(person => person.Name == name).FirstOrDefault();
+
+      return View(matchingPerson);
     }
   }
 }
