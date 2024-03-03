cc6b148b50c28e63275c7eada147898a05383445
Sebastian <mrseb10@gmail.com>
2024-03-03 17:11:19 +0100

    Weather App

---
+++ 6. Views/WeatherAppExercise/Models/City.cs
@@ -0,0 +1,18 @@
+﻿namespace WeatherAppExercise.Models
+{
+  public class City
+  {
+    public string UniqueCode { get; set; }
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public int TemperatureCelcius { get; set; }
+
+    public City(string uniqueCode, string name, DateTime date, int temperatureCelcius)
+    {
+      UniqueCode = uniqueCode;
+      Name = name;
+      Date = date;
+      TemperatureCelcius = temperatureCelcius;
+    }
+  }
+}
