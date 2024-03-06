56fafc590b4937487285b32ecaa21bd0dcff5844
BBualdo <mrseb10@gmail.com>
2024-03-06 14:07:03 +0100

    Partial Views with ViewData

--- 8. PartialViews/Controllers/HomeController.cs
+++ 8. PartialViews/Controllers/HomeController.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 
 public class HomeController : Controller {
+
     [Route("/")]
     public IActionResult Index() {
+        ViewData["ListTitle"] = "Citites";
+        ViewData["ListItems"] = new List<string>() {
+            "Kraków",
+            "Warszawa",
+            "Gdańsk",
+            "Wrocław",
+            "Lublin"
+        };
         return View();
+
     }
     [Route("about")]
     public IActionResult About() {
+        ViewData["ListTitle"] = "Employees";
+        ViewData["ListItems"] = new List<string>() {
+            "Asia",
+            "Sebastian",
+            "Dominik",
+            "Oliwia",
+            "Tomek"
+            };
         return View();
     }
 }
