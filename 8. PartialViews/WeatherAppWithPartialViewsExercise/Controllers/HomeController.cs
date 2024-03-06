f3a9db94ad59645756e2fde0e961f0aaf07d684f
BBualdo <mrseb10@gmail.com>
2024-03-06 16:45:42 +0100

    WeatherApp with Partial Views

---
+++ 8. PartialViews/WeatherAppWithPartialViewsExercise/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAppExercise.Models;
+
+namespace WeatherAppExercise.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/")]
+    public IActionResult Index()
+    {
+      List<City> cities = new List<City> {
+      new City("WWA", "Warszawa", DateTime.Now, 20),
+      new City("GDA", "Gdańsk", DateTime.Now, 40),
+      new City("KRK", "Krakówek", DateTime.Now, 12),
+      };
+
+      return View(cities);
+    }
+
+    [Route("details/{id}")]
+    public IActionResult Details(string? id)
+    {
+      if (id == null)
+      {
+        return Content("The id must be correct value.");
+      }
+
+      List<City> cities = new List<City> {
+      new City("WWA", "Warszawa", DateTime.Now, 20),
+      new City("GDA", "Gdańsk", DateTime.Now, 40),
+      new City("KRK", "Krakówek", DateTime.Now, 12),
+      };
+
+      City? matchingCity = cities.Where(city => city.UniqueCode == id).FirstOrDefault();
+
+      return View(matchingCity);
+    }
+  }
+}
