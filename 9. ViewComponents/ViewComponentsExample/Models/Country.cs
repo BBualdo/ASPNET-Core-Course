0390c41a1c87621c38db48dba5d965cbd572f876
BBualdo <mrseb10@gmail.com>
2024-03-06 22:58:37 +0100

    ViewData

---
+++ 9. ViewComponents/ViewComponentsExample/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿namespace ViewComponentsExample.Models;
+
+public class Country
+{
+  public string Name { get; set; }
+  public string CapitalCity { get; set; }
+
+  public Country(string name, string capital)
+  {
+    Name = name;
+    CapitalCity = capital;
+  }
+}
--- 9. ViewComponents/ViewComponentsExample/ViewComponents/GridViewComponent.cs
+++ 9. ViewComponents/ViewComponentsExample/ViewComponents/GridViewComponent.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
 
 namespace ViewComponentsExample;
 
 // [ViewComponent]
 public class GridViewComponent : ViewComponent
 {
-  public async Task<IViewComponentResult> InvokeAsync() {
+  public async Task<IViewComponentResult> InvokeAsync()
+  {
+    List<Country> countries = new List<Country>() {
+      new Country("Poland", "Warszawa"),
+      new Country("Greece", "Athenas"),
+      new Country("Germany", "Berlin"),
+      new Country("France", "Paris"),
+      new Country("Spain", "Madrid")
+    };
+    ViewBag.List = countries;
+
     return View(); // invoke partial view from /Views/Shared/Components/Grid/Default.cshtml
   }
 }
