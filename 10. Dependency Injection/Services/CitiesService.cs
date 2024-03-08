570e726ee42c3a8227eefabec5dd815b6b513ef5
Sebastian <mrseb10@gmail.com>
2024-03-08 19:05:13 +0100

    Services - 1/2

---
+++ 10. Dependency Injection/Services/CitiesService.cs
@@ -0,0 +1,24 @@
+﻿namespace Services
+{
+  public class CitiesService
+  {
+    private List<string> _citites;
+
+    public CitiesService()
+    {
+      _citites = new List<string>() {
+      "New York",
+      "Tokyo",
+      "Warsaw",
+      "Toronto",
+      "Valencia"
+      };
+    }
+
+    public List<string> GetCities()
+    {
+      return _citites;
+    }
+
+  }
+}
