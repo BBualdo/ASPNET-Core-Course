328e3867f9c5290db3331aac8ac9bf51521c70a8
Sebastian <mrseb10@gmail.com>
2024-03-09 10:51:30 +0100

    Method Injection - FromServices

--- 10. Dependency Injection/DIExample/Controllers/HomeController.cs
+++ 10. Dependency Injection/DIExample/Controllers/HomeController.cs
@@ -5,16 +5,18 @@ namespace DIExample.Controllers
 {
   public class HomeController : Controller
   {
+    /* Constructor Injection (used more often)
     private readonly ICitiesService _citiesService;
 
     public HomeController(ICitiesService citiesService)
     {
       // parameter provided by IoC via Dependency Injection
       _citiesService = citiesService;
-    }
+    } */
+
 
     [Route("/")]
-    public IActionResult Index()
+    public IActionResult Index([FromServices] ICitiesService _citiesService)
     {
       List<string> cities = _citiesService.GetCities();
       return View(cities);
