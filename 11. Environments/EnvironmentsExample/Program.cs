913289c6b1de0b5aeb5a3d104c6d6d80765bb2c4
Sebastian <mrseb10@gmail.com>
2024-03-10 21:26:29 +0100

    Environment in Launch Settings

--- 11. Environments/EnvironmentsExample/Program.cs
+++ 11. Environments/EnvironmentsExample/Program.cs
@@ -2,6 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
+if (
+  app.Environment.IsDevelopment() || // Environment = "Development"
+  app.Environment.IsStaging() || // Environment = "Staging"
+  app.Environment.IsEnvironment("Beta") // Environment = "Beta" (Custom Env)
+  )
+{
+  app.UseDeveloperExceptionPage(); // Showing detailed exception page for all env's except Production
+}
+
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers();
