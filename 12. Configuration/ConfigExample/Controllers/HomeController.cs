69eed6bcd5da8a3796aad29002ba9653b5b1361e
Sebastian <mrseb10@gmail.com>
2024-03-11 09:49:17 +0100

    IConfiguration in Controller

---
+++ 12. Configuration/ConfigExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigExample.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly IConfiguration _configuration;
+
+    public HomeController(IConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
+
+    [Route("config")]
+    public IActionResult Index()
+    {
+      ViewBag.Config = _configuration.GetValue<string>("MyKey");
+      return View();
+    }
+  }
+}
