3b62fda836a67e558608450b2976df0399f92d5d
Sebastian <mrseb10@gmail.com>
2024-03-11 13:28:21 +0100

    Custom Json Configuration

--- 12. Configuration/ConfigExample/Program.cs
+++ 12. Configuration/ConfigExample/Program.cs
@@ -4,6 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 // Supply an object of WeatherAPIOptions with "WeatherAPI" section as a service
 builder.Services.Configure<WeatherAPIOptions>(builder.Configuration.GetSection("WeatherAPI"));
+
+// Load Custom config
+builder.Configuration.AddJsonFile("MyOwnConfig.json", optional: true, reloadOnChange: true);
 var app = builder.Build();
 
 app.UseStaticFiles();
