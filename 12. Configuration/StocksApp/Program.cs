521597b85c91d114d76d05cc419c940e2e519b10
Sebastian <mrseb10@gmail.com>
2024-03-11 14:46:15 +0100

    Http Client - 1/3

--- 12. Configuration/StocksApp/Program.cs
+++ 12. Configuration/StocksApp/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
 app.UseStaticFiles();
---
+++ 12. Configuration/StocksApp/Services/MyService.cs
@@ -0,0 +1,26 @@
+﻿namespace StocksApp.Services
+{
+  public class MyService
+  {
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public MyService(IHttpClientFactory httpClientFactory)
+    {
+      _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task Method()
+    {
+      using (HttpClient client = _httpClientFactory.CreateClient())
+      {
+        HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+        {
+          RequestUri = new Uri("url"),
+          Method = HttpMethod.Get,
+        };
+
+        HttpResponseMessage httpResponseMessage = await client.SendAsync(httpRequestMessage);
+      }
+    }
+  }
+}
