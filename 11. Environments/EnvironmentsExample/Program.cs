52cdc3291c941c9774d28800aea045d2a3a49410
Sebastian <mrseb10@gmail.com>
2024-03-11 08:21:15 +0100

    Process Level Environment

--- 11. Environments/EnvironmentsExample/Program.cs
+++ 11. Environments/EnvironmentsExample/Program.cs
@@ -7,6 +7,10 @@ if (app.Environment.IsDevelopment())
   app.UseDeveloperExceptionPage();
 }
 
+/* In other place than IDE launchsetting.json doesnt exist, so in terminal to set Environment you have to write:
+  $Env:ASPNETCORE_ENVIRONMENT="Environment_Name"
+*/
+
 app.UseStaticFiles();
 app.UseRouting();
 app.MapControllers();
