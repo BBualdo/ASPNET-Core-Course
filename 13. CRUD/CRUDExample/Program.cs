660fc9deef73d688da68d7d519f027f17985452c
Sebastian <mrseb10@gmail.com>
2024-03-13 17:36:29 +0100

    Add Country - Dummy Service instance

--- 13. CRUD/CRUDExample/Program.cs
+++ 13. CRUD/CRUDExample/Program.cs
@@ -1,5 +1,9 @@
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ICountriesService, CountriesService>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
---
+++ 13. CRUD/CRUDTests/CountriesServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using ServiceContracts;
+
+namespace CRUDTests
+{
+  public class CountriesServiceTest
+  {
+    private readonly ICountriesService _countriesService;
+    public CountriesServiceTest(ICountriesService countriesService)
+    {
+      _countriesService = countriesService;
+    }
+  }
+}
