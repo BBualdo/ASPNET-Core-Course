4c481e8a9f2c121ae0643d8c060612334ec0d9b0
Sebastian <mrseb10@gmail.com>
2024-03-14 04:44:14 +0100

    AddCountry - Implementation

--- 13. CRUD/Services/CountriesService.cs
+++ 13. CRUD/Services/CountriesService.cs
@@ -1,13 +1,46 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services
 {
   public class CountriesService : ICountriesService
   {
+    private readonly List<Country> _countries;
+
+    public CountriesService()
+    {
+      _countries = new List<Country>();
+    }
+
     public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
     {
-      throw new NotImplementedException();
+      // Validation: Request can't be null
+      if (countryAddRequest == null)
+      {
+        throw new ArgumentNullException(nameof(countryAddRequest));
+      }
+      // Validation: CountryName can't be null
+      if (countryAddRequest.CountryName == null)
+      {
+        throw new ArgumentException(nameof(countryAddRequest.CountryName));
+      }
+
+      if (_countries.Where(country => country.CountryName == countryAddRequest.CountryName).Count() > 0)
+      {
+        throw new ArgumentException("Country already exists.");
+      }
+      // Converting request to Country Object
+      Country country = countryAddRequest.ToCountry();
+
+      // Adding CountryID
+      country.CountryID = Guid.NewGuid();
+
+      // Add country to "Database"
+      _countries.Add(country);
+
+      // Return CountryResponse
+      return country.ToCountryResponse();
     }
   }
 }
