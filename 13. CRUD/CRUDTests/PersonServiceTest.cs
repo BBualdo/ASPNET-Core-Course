6793e7db79ec59471a1112413ea235fc610d0fef
Sebastian <mrseb10@gmail.com>
2024-03-15 13:26:15 +0100

    AddPerson - TDD (OMO)

---
+++ 13. CRUD/CRUDTests/PersonServiceTest.cs
@@ -0,0 +1,66 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+
+namespace CRUDTests
+{
+  public class PersonServiceTest
+  {
+    private readonly IPersonService _personService;
+
+    public PersonServiceTest()
+    {
+      _personService = new PersonService();
+    }
+
+    [Fact]
+    public void AddPerson_NullPerson()
+    {
+      // Arrange
+      PersonAddRequest? personToAdd = null;
+
+      // Assert
+      Assert.Throws<ArgumentNullException>(() =>
+      {
+        // Act
+        _personService.AddPerson(personToAdd);
+      });
+    }
+
+    [Fact]
+    public void AddPerson_NoNamePerson()
+    {
+      // Arrange
+      PersonAddRequest personToAdd = new PersonAddRequest()
+      {
+        PersonName = null
+      };
+
+      // Assert
+      Assert.Throws<ArgumentException>(() =>
+      {
+        // Act
+        _personService.AddPerson(personToAdd);
+      });
+    }
+
+    [Fact]
+    public void AddPerson_CorrectPerson()
+    {
+      // Arrange
+      PersonAddRequest personToAdd = new PersonAddRequest()
+      {
+        PersonName = "Sebastian",
+        DateOfBirth = new DateTime(2000, 5, 23),
+        ReceivesNewsletter = true,
+        Address = "Example 23, 32-426 Atlantis"
+      };
+
+      // Act
+      PersonResponse addedPerson = _personService.AddPerson(personToAdd);
+
+      // Assert
+      Assert.True(addedPerson.PersonID != Guid.Empty);
+    }
+  }
+}
---
+++ 13. CRUD/Services/PersonService.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services
+{
+  public class PersonService : IPersonService
+  {
+    private readonly List<Person> _people;
+
+    public PersonService()
+    {
+      _people = new List<Person>();
+    }
+    public PersonResponse AddPerson(PersonAddRequest? personAddRequest)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
