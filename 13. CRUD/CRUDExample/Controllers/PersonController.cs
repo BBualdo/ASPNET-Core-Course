1ba34aeac5446935d2de799a0a1a26392a37da2d
Sebastian <mrseb10@gmail.com>
2024-03-19 07:23:27 +0100

    Getting Started with UI

---
+++ 13. CRUD/CRUDExample/Controllers/PersonController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDExample.Controllers
+{
+  public class PersonController : Controller
+  {
+    [Route("/people/index")]
+    [Route("/")]
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
